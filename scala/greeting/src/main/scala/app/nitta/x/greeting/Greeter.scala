b6024be946caef44ed2fb815192e5970e63fdeaa
kazukousen <mmchari.0228@gmail.com>
2021-09-08 01:27:03 +0900

    ide

--- scala/greeting/src/main/scala/app/nitta/x/greeting/Greeter.scala
+++ scala/greeting/src/main/scala/app/nitta/x/greeting/Greeter.scala
@@ -1,8 +1,12 @@
 package app.nitta.x.greeting
 
-import org.apache.lucene.store.RAMDirectory
+import java.nio.file.Files
+import org.apache.lucene.store.MMapDirectory
 
 object Greeter extends App {
-  val dir = new RAMDirectory
+  val dirPath = Files.createTempDirectory("lucene-index-")
+  val dir = new MMapDirectory(dirPath)
   println("Hello, world!")
+  dir.close()
+  Files.deleteIfExists(dirPath)
 }
