33f6c9623cf9cb1a624833cf5b0c3e9a63cf47da
kazukousen <mmchari.0228@gmail.com>
2021-09-08 03:31:28 +0900

    repl

--- tools/python/repl/repl.py
+++ tools/python/repl/repl.py
@@ -1,4 +1,18 @@
 import code
+import readline
+import rlcompleter
+import sys
 
 # https://docs.python.org/3/library/code.html
-code.interact(local=locals())
+
+vs = globals()
+vs.update(locals())
+readline.set_completer(rlcompleter.Completer(vs).complete)
+if sys.platform.startswith('linux'):
+    print("use linux...")
+    readline.parse_and_bind('tab: complete')
+elif sys.platform == 'darwin':
+    print("use darwin...")
+    readline.parse_and_bind('bind ^I rl_complete')
+
+code.InteractiveConsole(vs).interact()
