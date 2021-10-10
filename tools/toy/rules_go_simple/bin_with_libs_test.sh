4ea0bb44ffff17da2a1fa3f3bf2c5ca6ae43400b
kazukousen <mmchari.0228@gmail.com>
2021-10-11 01:18:05 +0900

    bin with libs test

---
+++ tools/toy/rules_go_simple/bin_with_libs_test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+program="$1"
+got=$("$program")
+want="foo
+bar
+baz
+baz"
+
+if [ "$got" != "$want" ]; then
+  cat >&2 <<EOF
+got:
+$got
+want:
+$want
+EOF
+  exit 1
+fi
