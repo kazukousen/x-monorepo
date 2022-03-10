e37e1b3b8212d184b7c81e24a61cc4201de3842b
kazukousen <mmchari.0228@gmail.com>
2022-03-10 23:16:16 +0900

    interrupts

--- rust/blog_os/src/main.rs
+++ rust/blog_os/src/main.rs
@@ -5,7 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use blog_os::{println, test_panic_handler};
+use blog_os::{println, test_panic_handler, init};
 
 fn main() {}
 
@@ -27,9 +27,15 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
