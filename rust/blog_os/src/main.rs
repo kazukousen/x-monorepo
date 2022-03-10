1edd1a791837a7d96a005910b6c351bb2d4cb8eb
kazukousen <mmchari.0228@gmail.com>
2022-03-10 23:29:59 +0900

    add double fault handler

--- rust/blog_os/src/main.rs
+++ rust/blog_os/src/main.rs
@@ -29,8 +29,10 @@ pub extern "C" fn _start() -> ! {
 
     init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    // cause page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    }
 
     #[cfg(test)]
     test_main();
