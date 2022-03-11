80d91ae775935d48199c5c78bcbec51e6ce642c4
kazukousen <mmchari.0228@gmail.com>
2022-03-12 02:07:06 +0900

    OffsetPageTable mapper

--- rust/blog_os/src/main.rs
+++ rust/blog_os/src/main.rs
@@ -7,7 +7,7 @@
 use blog_os::{println, test_panic_handler};
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
-use x86_64::structures::paging::PageTable;
+use x86_64::structures::paging::{PageTable, Translate};
 
 fn main() {}
 
@@ -27,32 +27,33 @@ fn panic(info: &PanicInfo) -> ! {
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use blog_os::memory::active_level_4_table;
+
+    use blog_os::memory;
     use x86_64::VirtAddr;
 
     println!("Hello World{}", "!");
 
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
-
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-
-            let phys = entry.frame().unwrap().start_address();
-            let virt = boot_info.physical_memory_offset + phys.as_u64();
-            let ptr: *mut PageTable = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
-    }
-
     blog_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
+
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
