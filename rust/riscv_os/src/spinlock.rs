ed8f5761c0633ebe70a69dd2672e3969b5906ab9
kazukousen <mmchari.0228@gmail.com>
2022-03-25 02:48:32 +0900

    wip

--- rust/riscv_os/src/spinlock.rs
+++ rust/riscv_os/src/spinlock.rs
@@ -12,21 +12,38 @@ pub struct SpinLock<T: ?Sized> {
 
 unsafe impl<T: ?Sized + Send> Sync for SpinLock<T> {}
 
-impl<T: Sized> SpinLock<T> {
+
+impl<T> SpinLock<T> {
     pub const fn new(data: T) -> Self {
         Self {
             lock: AtomicBool::new(false),
             data: UnsafeCell::new(data),
         }
     }
+}
 
-    pub fn acquire(&mut self) {
+impl<T: ?Sized> SpinLock<T> {
+
+    pub fn lock(&self) -> SpinLockGuard<'_, T> {
+        self.acquire();
+        SpinLockGuard {
+            lock: &self,
+            data: unsafe { &mut *self.data.get() },
+        }
+    }
+
+    fn acquire(&self) {
         while self
             .lock
             .compare_exchange(false, true, Ordering::Acquire, Ordering::Acquire)
             .is_err()
         {}
         fence(Ordering::SeqCst);
+    }
+
+    fn release(&self) {
+        fence(Ordering::SeqCst);
+        self.lock.store(false, Ordering::Release);
     }
 }
 
@@ -41,3 +58,16 @@ impl<'a, T: ?Sized> Deref for SpinLockGuard<'a, T> {
         &*self.data
     }
 }
+
+impl<'a, T: ?Sized> DerefMut for SpinLockGuard<'a, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut *self.data
+    }
+}
+
+impl <'a, T: ?Sized> Drop for SpinLockGuard<'a, T> {
+    fn drop(&mut self) {
+        self.lock.release();
+    }
+}
+
