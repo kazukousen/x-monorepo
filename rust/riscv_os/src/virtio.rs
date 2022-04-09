9277b12059db3f19ef7a1c49372a70e026930c31
kazukousen <mmchari.0228@gmail.com>
2022-04-09 12:44:12 +0900

    wip virtio

--- rust/riscv_os/src/virtio.rs
+++ rust/riscv_os/src/virtio.rs
@@ -1,6 +1,10 @@
+/// driver for qemu's virtio disk device.
+/// uses qemu's mmio interface to virtio.
+/// qemu presents a "legacy" virtio interface.
 use core::ptr;
 
-use crate::param::{self, VIRTIO0};
+use crate::param::{PAGESIZE, VIRTIO0};
+use array_macro::array;
 
 #[inline]
 unsafe fn read(offset: usize) -> u32 {
@@ -14,53 +18,148 @@ unsafe fn write(offset: usize, v: u32) {
     ptr::write_volatile(dst, v);
 }
 
-unsafe fn disk_init() {
-    if read(VIRTIO_MMIO_MAGIC_VALUE) != 0x74726976
-        || read(VIRTIO_MMIO_VERSION) != 1
-        || read(VIRTIO_MMIO_DEVICE_ID) != 2
-        || read(VIRTIO_MMIO_VENDOR_ID) != 0x554d4551
-    {
-        panic!("could not find virtio disk");
+#[repr(align(4096))]
+pub struct Disk {
+    align1: PageAlign,
+    desc: Desc,
+    avail: Avail,
+    used: Used,
+}
+
+impl Disk {
+    fn new() -> Self {
+        Self {
+            align1: PageAlign::new(),
+            desc: Desc::new(),
+            avail: Avail::new(),
+            used: Used::new(),
+        }
     }
+    pub unsafe fn init(&self) {
+        if read(VIRTIO_MMIO_MAGIC_VALUE) != 0x74726976
+            || read(VIRTIO_MMIO_VERSION) != 1
+            || read(VIRTIO_MMIO_DEVICE_ID) != 2
+            || read(VIRTIO_MMIO_VENDOR_ID) != 0x554d4551
+        {
+            panic!("could not find virtio disk");
+        }
 
-    let mut status: u32 = 0;
-    status |= VIRTIO_CONFIG_S_ACKNOWLEDGE;
-    write(VIRTIO_MMIO_STATUS, status);
-    status |= VIRTIO_CONFIG_S_DRIVER;
-    write(VIRTIO_MMIO_STATUS, status);
+        let mut status: u32 = 0;
+        status |= VIRTIO_CONFIG_S_ACKNOWLEDGE;
+        write(VIRTIO_MMIO_STATUS, status);
+        status |= VIRTIO_CONFIG_S_DRIVER;
+        write(VIRTIO_MMIO_STATUS, status);
 
-    // negotiate features
-    let mut features = read(VIRTIO_MMIO_DEVICE_FEATURES);
-    features &= !(1 << VIRTIO_BLK_F_RO);
-    features &= !(1 << VIRTIO_BLK_F_SCSI);
-    features &= !(1 << VIRTIO_BLK_F_CONFIG_WCE);
-    features &= !(1 << VIRTIO_BLK_F_MQ);
-    features &= !(1 << VIRTIO_F_ANY_LAYOUT);
-    features &= !(1 << VIRTIO_RING_F_EVENT_IDX);
-    features &= !(1 << VIRTIO_RING_F_INDIRECT_DESC);
-    write(VIRTIO_MMIO_DEVICE_FEATURES, features);
+        // negotiate features
+        let mut features = read(VIRTIO_MMIO_DEVICE_FEATURES);
+        features &= !(1 << VIRTIO_BLK_F_RO);
+        features &= !(1 << VIRTIO_BLK_F_SCSI);
+        features &= !(1 << VIRTIO_BLK_F_CONFIG_WCE);
+        features &= !(1 << VIRTIO_BLK_F_MQ);
+        features &= !(1 << VIRTIO_F_ANY_LAYOUT);
+        features &= !(1 << VIRTIO_RING_F_EVENT_IDX);
+        features &= !(1 << VIRTIO_RING_F_INDIRECT_DESC);
+        write(VIRTIO_MMIO_DEVICE_FEATURES, features);
 
-    // tell device that feature negotiation is complete.
-    status |= VIRTIO_CONFIG_S_FEATURE_OK;
-    write(VIRTIO_MMIO_STATUS, status);
+        // tell device that feature negotiation is complete.
+        status |= VIRTIO_CONFIG_S_FEATURE_OK;
+        write(VIRTIO_MMIO_STATUS, status);
 
-    // tell device we're complete ready.
-    status |= VIRTIO_CONFIG_S_DRIVER_OK;
-    write(VIRTIO_MMIO_STATUS, status);
+        // tell device we're complete ready.
+        status |= VIRTIO_CONFIG_S_DRIVER_OK;
+        write(VIRTIO_MMIO_STATUS, status);
 
-    write(VIRTIO_MMIO_GUEST_PAGE_SIZE, param::PAGESIZE as u32);
+        write(VIRTIO_MMIO_GUEST_PAGE_SIZE, PAGESIZE as u32);
 
-    // initialize queue 0.
-    write(VIRTIO_MMIO_QUEUE_SEL, 0);
-    let max: u32 = read(VIRTIO_MMIO_QUEUE_NUM_MAX);
-    if max == 0 {
-        panic!("virtio disk has no queue 0");
-    } else if max < NUM {
-        panic!("virtio disk max queue too short");
+        // initialize queue 0.
+        write(VIRTIO_MMIO_QUEUE_SEL, 0);
+        let max: u32 = read(VIRTIO_MMIO_QUEUE_NUM_MAX);
+        if max == 0 {
+            panic!("virtio disk has no queue 0");
+        } else if max < NUM {
+            panic!("virtio disk max queue too short");
+        }
+        write(VIRTIO_MMIO_QUEUE_NUM, NUM);
+
+        let pfn: usize = self as *const Disk as usize >> 12;
+        write(VIRTIO_MMIO_QUEUE_PFN, u32::try_from(pfn).unwrap());
     }
-    write(VIRTIO_MMIO_QUEUE_NUM, NUM);
+}
 
-    // TODO
+#[repr(align(4096))]
+struct PageAlign();
+
+impl PageAlign {
+    fn new() -> Self {
+        Self()
+    }
+}
+
+#[repr(C)]
+struct Desc {
+    addr: usize,
+    len: u32,
+    flags: u16,
+    next: u16,
+}
+
+impl Desc {
+    fn new() -> Self {
+        Self {
+            addr: 0,
+            len: 0,
+            flags: 0,
+            next: 0,
+        }
+    }
+}
+
+#[repr(C)]
+struct Avail {
+    flags: u16,
+    idx: u16,
+    ring: [u16; NUM as usize],
+    unused: u16,
+}
+
+impl Avail {
+    fn new() -> Self {
+        Self {
+            flags: 0,
+            idx: 0,
+            ring: array![_ => 0_u16; NUM as usize],
+            unused: 0,
+        }
+    }
+}
+
+#[repr(C)]
+struct Used {
+    flags: u16,
+    idx: u16,
+    ring: [UsedElem; NUM as usize],
+}
+
+impl Used {
+    fn new() -> Self {
+        Self {
+            flags: 0,
+            idx: 0,
+            ring: array![_ => UsedElem::new(); NUM as usize],
+        }
+    }
+}
+
+#[repr(C)]
+struct UsedElem {
+    id: u32,
+    len: u32,
+}
+
+impl UsedElem {
+    fn new() -> Self {
+        Self { id: 0, len: 0 }
+    }
 }
 
 const VIRTIO_MMIO_MAGIC_VALUE: usize = 0x000;
